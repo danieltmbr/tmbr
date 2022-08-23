557445de26c46e026bc5632b68daeca6fb59a6dd
Daniel Tombor <t.dani12@gmail.com>
2022-08-23 13:09:49 +0100

    Init Vapor app

---
+++ web/Sources/App/configure.swift
@@ -0,0 +1,8 @@
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    // register routes
+    try routes(app)
+}
