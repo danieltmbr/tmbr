f6ad7bbcd30a7dad1fb2f402aecc479287ee523b
Daniel Tombor <t.dani12@gmail.com>
2025-04-04 07:25:33 +0100

    Update database configuration for Heroku

--- tmbr/Sources/App/Configurations/Configure+Database.swift
+++ tmbr/Sources/App/Configurations/Configure+Database.swift
@@ -3,15 +3,8 @@ import FluentPostgresDriver
 import Vapor
 
 func configureDatabase(_ app: Application) throws {
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.database.hostname,
-        port: Environment.database.port,
-        username: Environment.database.username,
-        password: Environment.database.password,
-        database: Environment.database.database,
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-
+    try app.databases.use(Environment.database.config, as: .psql)
+    
     app.migrations.add(CreatePost())
     app.migrations.add(CreateUser())
 }
--- tmbr/Sources/App/Environment/Environment+Database.swift
+++ tmbr/Sources/App/Environment/Environment+Database.swift
@@ -12,6 +12,27 @@ extension Environment {
         let password = Environment.get("DATABASE_PASSWORD")!
         
         let database = Environment.get("DATABASE_NAME")!
+        
+        var config: DatabaseConfigurationFactory {
+            get throws {
+                let databaseURL: URL
+                let tlsConfiguration: PostgresConnection.Configuration.TLS
+                
+                if let url = Environment.get("DATABASE_URL").flatMap(URL.init(string:)) {
+                    databaseURL = url
+                    var config: TLSConfiguration = .makeClientConfiguration()
+                    config.certificateVerification = .none
+                    tlsConfiguration = try .require(NIOSSLContext(configuration: config))
+                } else {
+                    databaseURL = URL(string: "postgresql://\(username):\(password)@\(hostname):\(port)/\(database)")!
+                    tlsConfiguration = try .prefer(NIOSSLContext(configuration: .clientDefault))
+                }
+                
+                var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+                postgresConfig.coreConfiguration.tls = tlsConfiguration
+                return .postgres(configuration: postgresConfig)
+            }
+        }
     }
 
     /// Evironment values for setting up the Database
