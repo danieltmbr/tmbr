25f43b6c094a78b11ff26abf6f2fb5dbeb997a9c
Daniel Tombor <t.dani12@gmail.com>
2025-12-03 16:30:45 +0000

    Media Response models (#70)

---
+++ tmbr/Sources/App/Modules/Catalogue/Routes/API/Responses/BookResponse.swift
@@ -0,0 +1,79 @@
+import Vapor
+import Foundation
+import AuthKit
+
+struct BookResponse: Encodable, Sendable {
+    
+    private let id: Int
+    
+    private let access: Access
+    
+    private let author: String
+    
+    private let cover: ImageResponse?
+    
+    private let genre: String
+    
+    private let notes: [NoteResponse]
+    
+    private let owner: UserResponse
+    
+    private let preview: PreviewResponse
+    
+    private let post: PostResponse?
+    
+    private let releaseDate: Date
+    
+    private let resources: [Resource]
+
+    private let title: String
+    
+    init(
+        id: Int,
+        access: Access,
+        author: String,
+        cover: ImageResponse?,
+        genre: String,
+        notes: [NoteResponse],
+        owner: UserResponse,
+        preview: PreviewResponse,
+        post: PostResponse?,
+        releaseDate: Date,
+        resources: [Resource],
+        title: String
+    ) {
+        self.id = id
+        self.access = access
+        self.author = author
+        self.cover = cover
+        self.genre = genre
+        self.notes = notes
+        self.owner = owner
+        self.preview = preview
+        self.post = post
+        self.releaseDate = releaseDate
+        self.resources = resources
+        self.title = title
+    }
+    
+    init(
+        book: Book,
+        baseURL: String,
+        platform: Platform<Book> = .all
+    ) {
+        self.init(
+            id: book.id!,
+            access: book.access,
+            author: book.author,
+            cover: book.cover.map { ImageResponse(image: $0, baseURL: baseURL) },
+            genre: book.genre,
+            notes: book.notes.map { NoteResponse(note: $0, baseURL: baseURL) },
+            owner: UserResponse(user: book.owner),
+            preview: PreviewResponse(preview: book.preview, baseURL: baseURL),
+            post: book.post.map { PostResponse(post: $0, baseURL: baseURL) },
+            releaseDate: book.releaseDate,
+            resources: book.resourceURLs.compactMap(platform.resource),
+            title: book.title
+        )
+    }
+}
---
+++ tmbr/Sources/App/Modules/Catalogue/Routes/API/Responses/MovieResponse.swift
@@ -0,0 +1,79 @@
+import Vapor
+import Foundation
+import AuthKit
+
+struct MovieResponse: Encodable, Sendable {
+    
+    private let id: Int
+    
+    private let access: Access
+    
+    private let cover: ImageResponse?
+    
+    private let director: String?
+    
+    private let genre: String?
+    
+    private let notes: [NoteResponse]
+    
+    private let owner: UserResponse
+    
+    private let preview: PreviewResponse
+    
+    private let post: PostResponse?
+    
+    private let releaseDate: Date?
+    
+    private let resources: [Resource]
+    
+    private let title: String
+    
+    init(
+        id: Int,
+        access: Access,
+        cover: ImageResponse?,
+        director: String?,
+        genre: String?,
+        notes: [NoteResponse],
+        owner: UserResponse,
+        preview: PreviewResponse,
+        post: PostResponse?,
+        releaseDate: Date?,
+        resources: [Resource],
+        title: String
+    ) {
+        self.id = id
+        self.access = access
+        self.cover = cover
+        self.director = director
+        self.genre = genre
+        self.notes = notes
+        self.owner = owner
+        self.preview = preview
+        self.post = post
+        self.releaseDate = releaseDate
+        self.resources = resources
+        self.title = title
+    }
+    
+    init(
+        movie: Movie,
+        baseURL: String,
+        platform: Platform<Movie> = .all
+    ) {
+        self.init(
+            id: movie.id!,
+            access: movie.access,
+            cover: movie.cover.map { ImageResponse(image: $0, baseURL: baseURL) },
+            director: movie.director,
+            genre: movie.genre,
+            notes: movie.notes.map { NoteResponse(note: $0, baseURL: baseURL) },
+            owner: UserResponse(user: movie.owner),
+            preview: PreviewResponse(preview: movie.preview, baseURL: baseURL),
+            post: movie.post.map { PostResponse(post: $0, baseURL: baseURL) },
+            releaseDate: movie.releaseDate,
+            resources: movie.resourceURLs.compactMap(platform.resource),
+            title: movie.title
+        )
+    }
+}
