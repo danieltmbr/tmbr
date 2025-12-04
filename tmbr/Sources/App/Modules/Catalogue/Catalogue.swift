5efb5484a1464107d5e6cdf860e8399b9cd7cb0e
Daniel Tombor <t.dani12@gmail.com>
2025-12-04 13:58:27 +0000

    Media Item permissions

--- tmbr/Sources/App/Modules/Catalogue/Catalogue.swift
+++ tmbr/Sources/App/Modules/Catalogue/Catalogue.swift
@@ -1,9 +1,25 @@
 import Vapor
 import Fluent
 import Core
-import SotoCore
+import AuthKit
 
 struct Catalogue: Module {
+    
+    private let permissions: [PermissionScope]
+    
+    init(
+        bookPermissions: PermissionScope,
+        moviePermissions: PermissionScope,
+        podcastPermissions: PermissionScope,
+        songPermissions: PermissionScope
+    ) {
+        self.permissions = [
+            bookPermissions,
+            moviePermissions,
+            podcastPermissions,
+            songPermissions
+        ]
+    }
 
     func configure(_ app: Vapor.Application) async throws {
         app.migrations.add(CreateBook())
@@ -22,6 +38,10 @@ struct Catalogue: Module {
         app.databases.middleware.use(PreviewModelMiddleware.movie, on: .psql)
         app.databases.middleware.use(PreviewModelMiddleware.podcast, on: .psql)
         app.databases.middleware.use(PreviewModelMiddleware.song, on: .psql)
+        
+        for scope in permissions {
+            try await app.permissions.add(scope: scope)
+        }
     }
     
     func boot(_ routes: any Vapor.RoutesBuilder) async throws {
@@ -29,5 +49,12 @@ struct Catalogue: Module {
 }
 
 extension Module where Self == Catalogue {
-    static var catalogue: Self { Catalogue() }
+    static var catalogue: Self {
+        Catalogue(
+            bookPermissions: PreviewablePermissionScope.books,
+            moviePermissions: PreviewablePermissionScope.movies,
+            podcastPermissions: PreviewablePermissionScope.podcasts,
+            songPermissions: PreviewablePermissionScope.songs
+        )
+    }
 }
