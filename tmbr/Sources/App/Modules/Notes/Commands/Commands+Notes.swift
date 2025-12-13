9dd3bc48576c997aea5ab8ccf0ea55107a95d27e
Daniel Tombor <t.dani12@gmail.com>
2025-12-13 16:59:22 +0000

    Add new Note commands (#84)

--- tmbr/Sources/App/Modules/Notes/Commands/Commands+Notes.swift
+++ tmbr/Sources/App/Modules/Notes/Commands/Commands+Notes.swift
@@ -7,24 +7,32 @@ extension Commands {
 
 extension Commands {
     struct Notes: CommandCollection, Sendable {
-                
+        
+        let batchCreate: CommandFactory<BatchCreateNoteInput, [Note]>
+        
         let create: CommandFactory<CreateNoteInput, Note>
         
         let delete: CommandFactory<NoteID, Void>
                 
         let edit: CommandFactory<EditNoteInput, Note>
         
+        let query: CommandFactory<QueryNotesInput, [Note]>
+        
         let search: CommandFactory<NoteQueryPayload, [Note]>
         
         init(
+            batchCreate: CommandFactory<BatchCreateNoteInput, [Note]> = .createNotes,
             create: CommandFactory<CreateNoteInput, Note> = .createNote,
             delete: CommandFactory<NoteID, Void> = .deleteNote,
             edit: CommandFactory<EditNoteInput, Note> = .editNote,
+            query: CommandFactory<QueryNotesInput, [Note]> = .queryNotes,
             search: CommandFactory<NoteQueryPayload, [Note]> = .searchNote
         ) {
+            self.batchCreate = batchCreate
             self.create = create
             self.delete = delete
             self.edit = edit
+            self.query = query
             self.search = search
         }
     }
