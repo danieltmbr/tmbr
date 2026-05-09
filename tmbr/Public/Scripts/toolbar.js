c44c2a85abb9c1c01f530a36d290456920d0a2ee
Daniel Tombor <t.dani12@gmail.com>
2026-05-09 12:41:36 +0100

    Unify popup behaviour and placement.

--- tmbr/Public/Scripts/toolbar.js
+++ tmbr/Public/Scripts/toolbar.js
@@ -6,32 +6,36 @@ class ToolbarController {
         this.newItemsPanel = newItemsPanel;
         this._onNewItemClick = this.openNewItemsPanel.bind(this);
         this._onCloseClick = this.closeNewItemsPanel.bind(this);
+        this._onOutsideClick = this._handleOutsideClick.bind(this);
     }
-    
+
     init() {
         this.newItemButton.addEventListener('click', this._onNewItemClick);
         this.newItemCloseButton.addEventListener('click', this._onCloseClick);
     }
-    
+
     destroy() {
         this.newItemButton.removeEventListener('click', this._onNewItemClick);
         this.newItemCloseButton.removeEventListener('click', this._onCloseClick);
+        document.removeEventListener('click', this._onOutsideClick);
     }
-    
-    toggle() {
-        if (this.newItemsPanel.classList.contains("open")) {
-            this.close();
-        } else {
-            this.open();
-        }
-    }
-    
+
     openNewItemsPanel() {
         this.newItemsPanel.classList.add("open");
+        setTimeout(() => {
+            document.addEventListener('click', this._onOutsideClick);
+        }, 0);
     }
-    
+
     closeNewItemsPanel() {
         this.newItemsPanel.classList.remove("open");
+        document.removeEventListener('click', this._onOutsideClick);
+    }
+
+    _handleOutsideClick(e) {
+        if (!this.newItemsPanel.contains(e.target) && !this.newItemButton.contains(e.target)) {
+            this.closeNewItemsPanel();
+        }
     }
 }
 
